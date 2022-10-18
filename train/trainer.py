c34797c5df691beb81b8cd2efd75f9c42c0078bb
drmatthewclark <pi@mclark_1>
2022-10-18 08:58:24 -0400

    update

--- train/trainer.py
+++ train/trainer.py
@@ -21,66 +21,70 @@ errors = {}
 gap = 60
 
 def send(message):
-	client = mqtt.Client('trainer')
-	res = client.connect(host)
-	ecode, count  = client.publish(topic, message.encode('utf8'), qos)
+        client = mqtt.Client('trainer')
+        res = client.connect(host)
+        ecode, count  = client.publish(topic, message.encode('utf8'), qos)
 
-def readwords(wordfile):
-	with open(wordfile, 'r') as file:
-		for word in file:
-			wordlist.append(word.strip())
+def readwords(files):
+        for wordfile in files[1:]:
+            with open(wordfile, 'r') as file:
+                for word in file:
+                   wordlist.append(word.strip())
 
 
 def savehist():
-	with open(historyfile, 'wb') as f:
-		pickle.dump(errors, f)
+        with open(historyfile, 'wb') as f:
+                pickle.dump(errors, f)
 
 def readhist():
-	global errors
-	try:
-		errors = pickle.load(historyfile)
-		print('errors', errors )
-	except:
-		pass
+        global errors
+        try:
+                errors = pickle.load(historyfile)
+                print('errors', errors )
+        except:
+                pass
 
-def train(wordfile):
+def train(files):
 
-	count = 0
-	random.seed(seed)
-	readwords(wordfile)
-	print('read', len(wordlist), 'words' )
-	while True:
-		count += 1
-		nextword = random.choice(wordlist)
-		send(nextword)
-		time.sleep(3)
-		send(nextword)
-		user = input('word:')
-		if user == '@':
-			savehist()
-			exit(0)
-	
-		if user.upper() == nextword.upper():
-			print('correct!')
-			if nextword in errors:
-				errors[nextword] -= 1
-				if errors[nextword] == 0:
-					del errors[nextword]
-		else:
-			print('word was ', nextword )
-			if nextword in errors:
-				errors[nextword] += 1
-			else:
-				errors[nextword] = 1
+        count = 0
+        readwords(files)
+        print('read', len(wordlist), 'words' )
+        while True:
+                count += 1
+                nextword = random.choice(wordlist)
+                send(nextword)
+                time.sleep(3)
+                send(nextword)
+                user = input('word:')
+                if user == '@':
+                        savehist()
+                        exit(0)
+        
+                if user.upper() == nextword.upper():
+                        print('correct!')
+                        if 'correct' not in errors:
+                                errors['correct'] = 0
+                        errors['correct'] += 1
 
-		print(errors)
-		savehist()
-		if len(errors) > 5 and count % 10 == 0:
-			for e in errors:
-				print('char', e )
-				send(e)
-				time.sleep(2)
-		time.sleep(1)
+                        if nextword in errors:
+                                errors[nextword] -= 1
+                                if errors[nextword] == 0:
+                                        del errors[nextword]
+                else:
+                        print('word was ', nextword )
+                        if nextword in errors:
+                                errors[nextword] += 1
+                        else:
+                                errors[nextword] = 1
+
+                print(errors)
+                savehist()
+                if len(errors) > 5 and count % 50 == 0:
+                        for e in errors:
+                                print('char', e )
+                                send(e)
+                                time.sleep(2)
+                time.sleep(1)
 
 readhist()
-train(sys.argv[1])
+train(sys.argv)
