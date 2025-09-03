cce23640c95ab352e770259606d317795735aa38
matt <mclark@drmatthewclark.com>
2025-09-02 21:48:15 -0400

    make config file template so as not to overwrite

---
+++ config_template.py
@@ -0,0 +1,27 @@
+"""
+configure which pins the equipment is connected to
+"""
+
+import syslog
+import RPi.GPIO as GPIO
+import syslog
+
+gpioMode = GPIO.BOARD
+qos = 0
+
+gpioOutputPin=   12
+gpioInputPin =   18
+gpioInputGnd = False  # signal is grounding gpio pin, vs connecting to 3.3v
+
+wpm = 20
+MAX_WPM = 100
+
+randomAmount = 0.02 
+log_level = syslog.LOG_INFO
+
+# IP addresses for keys to send signals to
+
+IPS = ['localhost']
+
+
+loglevels = { 'LOG_EMERG':0, 'LOG_ALERT':1, 'LOG_CRIT' : 2, 'LOG_ERR': 3, 'LOG_WARNING':4, 'LOG_NOTICE':5, 'LOG_INFO':6, 'LOG_DEBUG' : 7 }
