cf10807d04a75d7cac3951a4605c1a18f4db90cd
matt <mclark@drmatthewclark.com>
2026-04-21 13:04:04 -0400

    update

--- telegraph_listener.py
+++ telegraph_listener.py
@@ -124,7 +124,7 @@ def on_disconnect(client, userdata, rs, properties):
     host = client._host
     logmesg(syslog.LOG_ERR, f'on_disconnect: {client} {rs} {host}  disconnected')
 
-    ret = client.connect(host)
+    ret = client.connect(host=host, port=TPORT)
 
     if ret == 0:
         logmesg(syslog.LOG_INFO, f'on_disconnect: reconnected {host}')
@@ -147,7 +147,7 @@ def setup():
        message_client.on_message = on_message 
        message_client.on_connect = on_connect
        message_client.on_disconnect = on_disconnect
-       message_client.connect(IP)
+       message_client.connect(host=IP, port=TPORT)
        message_client.loop_start()  # Start networking daemon
 
 
