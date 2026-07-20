b900a491db0d589d176cce05dde75e1647c11058
matt <mclark@drmatthewclark.com>
2026-07-20 11:13:25 -0400

    reboot after update

--- maintain.sh
+++ maintain.sh
@@ -3,11 +3,11 @@
 
 cd /usr/local/rpi_telegraph
 git pull
-service telegraph_listener restart
-service key_listener restart
+#service telegraph_listener restart
+#service key_listener restart
 
 cd /var/www/html
 git pull
 
-pm2 restart all
+#pm2 restart all
 reboot
