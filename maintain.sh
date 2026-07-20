896283623df53761f7770bbdbbc1aa05ef68dfd2
matt <mclark@drmatthewclark.com>
2026-07-20 11:11:59 -0400

    add maintenance script

---
+++ maintain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# periodic auto-update
+
+cd /usr/local/rpi_telegraph
+git pull
+service telegraph_listener restart
+service key_listener restart
+
+cd /var/www/html
+git pull
+
+pm2 restart all
+reboot
