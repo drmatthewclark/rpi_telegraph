e37338f0d2ddc2ada1e6a6c9a410961c413b83ed
matt <mclark@drmatthewclark.com>
2026-07-22 10:35:14 -0400

    update

--- maintain.sh
+++ maintain.sh
@@ -5,13 +5,20 @@ echo "start maintainance"
 date
 
 cd /usr/local/rpi_telegraph
+echo "update crontab"
 crontab crontab
+pwd
 git pull
 
 cd /var/www/html
+pwd
 git pull
 
+cd /root
 sync
 sync
+
+echo -n "reboot "
+date
 
 /usr/sbin/reboot -f
